a22606f3311320657623194f945a995da3475e93
miftahudin19 <40747706+miftahudin19@users.noreply.github.com>
2018-12-17 21:39:00 +0700

    Add files via upload

---
+++ Program Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int r,luas;
+	cout <<"Input Jari-jari : ";
+	cin >>r;
+	
+	luas = 3.12 * r * r;
+	cout<<"Luas Alas Lingkarannya Adalah = " << luas;
+	
+	return 0;
+}
